70cd8be724527d8f9014b361adf2e9be960b88c5
zrvnarmarin <76marin76@gmail.com>
2022-12-13 23:45:43 +0100

    patients added

--- test-react/src/App.jsx
+++ test-react/src/App.jsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+
 // import { createContext, useState, useContext } from 'react'
 
 // export const MovieContext = createContext()
@@ -94,154 +95,11 @@
 
 // export default App
 
-import React, { useState, useContext, useReducer } from 'react'
-
-const ACTIONS = {
-	SET_NAME: 'set name',
-	RESET_NAME: 'reset name',
-	SET_SURNAME: 'set name',
-	RESET_SURNAME: 'reset name',
-	SET_GENDER: 'set name',
-	RESET_GENDER: 'reset name',
-	SET_IDENTIFICATION_NUMBER: 'set name',
-	RESET_IDENTIFICATION_NUMBER: 'reset name',
-	SET_ROOM: 'set name',
-	RESET_ROOM: 'reset name',
-	SET_DEPARTMENT: 'set name',
-	RESET_DEPARTMENT: 'reset name',
-	ADD_PATIENT: 'add patient'
-}
-
-const reducer = (state, action) => {
-	switch(action.type) {
-		case ACTIONS.SET_NAME: {
-			return {
-				...state,
-				name: action.payload
-			}
-		}
-		case ACTIONS.RESET_NAME: {
-			return {
-				...state,
-				name: ''
-			}
-		}
-		case ACTIONS.SET_SURNAME: {
-			return {
-				surname: action.payload,
-				...state
-			}
-		}
-		case ACTIONS.RESET_SURNAME: {
-			return {
-				...state,
-				surname: ''
-			}
-		}
-		case ACTIONS.SET_GENDER: {
-			return {
-				...state,
-				gender: action.payload
-			}
-		}
-		case ACTIONS.RESET_GENDER: {
-			return {
-				...state,
-				gender: ''
-			}
-		}
-		case ACTIONS.SET_IDENTIFICATION_NUMBER: {
-			return {
-				...state,
-				identificationNumber: action.payload
-			}
-		}
-		case ACTIONS.RESET_IDENTIFICATION_NUMBER: {
-			return {
-				...state,
-				identificationNumber: ''
-			}
-		}
-		case ACTIONS.SET_ROOM: {
-			return {
-				...state,
-				room: action.payload
-			}
-		}
-		case ACTIONS.RESET_ROOM: {
-			return {
-				...state,
-				room: ''
-			}
-		}
-		case ACTIONS.SET_DEPARTMENT: {
-			return {
-				...state,
-				department: action.payload
-			}
-		}
-		case ACTIONS.RESET_DEPARTMENT: {
-			return {
-				...state,
-				department: ''
-			}
-		}
-		case ACTIONS.ADD_PATIENT: {
-			return { ...state, patients: [...state.patients, action.payload]}
-		}
-	}
-}
-
+import React from "react";
+import Patients from "./components/Patients";
 const App = () => {
-	const [state, dispatch] = useReducer(reducer, 
-		{ name: '', surname: '', gender: '', identificationNumber: '', roomNumber: '', department: '', patients: []}
-	)
-
-	const nameInputHandler = e => dispatch({ type: ACTIONS.SET_NAME, payload: e.target.value })
-	const surnameInputHandler = e => dispatch({ type: ACTIONS.SET_SURNAME, payload: e.target.value })
-	const genderInputHandler = e => dispatch({ type: ACTIONS.SET_GENDER, payload: e.target.value })
-	const identificationNumberInputHandler = e => dispatch({ type: ACTIONS.SET_IDENTIFICATION_NUMBER, payload: e.target.value })
-	const roomInputHandler = e => dispatch({ type: ACTIONS.SET_ROOM, payload: e.target.value })
-	const departmentInputHandler = e => dispatch({ type: ACTIONS.SET_DEPARTMENT, payload: e.target.value })
-
-
-	const formSubmitHandler = e => {
-		e.preventDefault()
-
-		dispatch({type: ACTIONS.ADD_PATIENT, payload: { 
-			id: Math.floor((1 + Math.random()) * 0x10000),
-			name: state.name, 
-			surname: state.surname,
-			gender: state.gender,
-			identificationNumber: state.identificationNumber,
-			room: state.room,
-			department: state.department
-		}})
-
-		resetFormInputs()
-	}
-
-	const resetFormInputs = () => {
-		dispatch({type: ACTIONS.RESET_NAME})
-		dispatch({type: ACTIONS.RESET_SURNAME})
-		dispatch({type: ACTIONS.RESET_GENDER})
-		dispatch({type: ACTIONS.RESET_IDENTIFICATION_NUMBER})
-		dispatch({type: ACTIONS.RESET_ROOM})
-		dispatch({type: ACTIONS.RESET_DEPARTMENT})
-	}
-
 	return (
-		<div>
-			<form onSubmit={formSubmitHandler} className='bg-red-400 flex flex-col gap-2 justify-center items-center'>
-				<input  onChange={genderInputHandler} type="text" placeholder='Unesi spol...' />
-				<input onChange={identificationNumberInputHandler} type="text" placeholder='Unesi matični broj...' />
-				<input  onChange={roomInputHandler} type="text" placeholder='Unesi broj sobe...' />
-				<input  onChange={departmentInputHandler} type="text" placeholder='Unesi unesi broj odjela...' />
-				<button>Submit</button>
-			</form>
-			NAME: {JSON.stringify(state.name)} <br />
-			SURNAME: {JSON.stringify(state.surname)}
-		</div>
+		<Patients />
 	)
 }
 
---
+++ test-react/src/components/Patients.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { useReducer } from 'react'
+
+const ACTIONS = {
+    SET_NAME: 'set name',
+    SET_NUMBER: 'set number',
+    ADD_PERSON: 'add person',
+    DELETE_PERSON: 'delete person'
+}
+
+function reducer (state, action) {
+    switch (action.type) {
+        case ACTIONS.SET_NAME: {
+            return { ...state, name: action.payload}
+        }
+        case ACTIONS.SET_NUMBER: {
+            return { ...state, number: action.payload}
+        }
+        case ACTIONS.ADD_PERSON: {
+            return { ...state, persons: [...state.persons, action.payload]}
+        }
+        case ACTIONS.DELETE_PERSON: {
+            return {...state, persons: state.persons.filter(person => person.id !== action.payload.id)}
+        }
+    }
+}
+
+const Patients = () => {
+    const [state, dispatch] = useReducer(reducer, { name: '', number: '', persons: []})
+    const nameHandler = (e) => {
+        dispatch({type: ACTIONS.SET_NAME, payload: e.target.value})
+    }
+    const numberHandler = (e) => {
+        dispatch({type: ACTIONS.SET_NUMBER, payload: e.target.value})
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch({type: ACTIONS.ADD_PERSON, payload: { 
+            id: Math.floor((1 + Math.random()) * 0x10000),  
+            name: state.name, 
+            number: state.number 
+        }})
+    }
+
+  return (
+    <div>
+        <div>
+            <form onSubmit={submitHandler}>
+                <input onChange={nameHandler} type="text" />
+                <input onChange={numberHandler} type="text" />
+                <button>Submit</button>
+            </form>
+            <br /> <br />
+            {state.persons.map(person => {
+                return <div key={person.id}>
+                    <p>{person.name}</p>
+                    <button onClick={() => dispatch({type: ACTIONS.DELETE_PERSON, payload: { id: person.id }})}>Delete PErson</button>
+                </div>
+            } )}
+        </div>
+    </div>
+  )
+}
+
+export default Patients
