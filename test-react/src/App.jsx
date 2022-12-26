0954a0af6fae47b34312f4b6c02176d14c4273c5
zrvnarmarin <76marin76@gmail.com>
2022-12-26 13:05:00 +0100

    addig context

--- test-react/src/App.jsx
+++ test-react/src/App.jsx
@@ -1,106 +1,54 @@
-// import React from 'react'
+import React, { useState, createContext, useContext } from 'react';
 
-// import { createContext, useState, useContext } from 'react'
+const MovieContext = createContext();
 
-// export const MovieContext = createContext()
+const MovieProvider = (props) => {
+	const [movies, setMovies] = useState([
+		{ id: 1, name: 'Tenet', price: '10'},
+		{ id: 2, name: 'Kill Bill 2', price: '20'},
+		{ id: 3, name: 'The dark knight', price: '40'}
+	])
 
-// export const MovieProvider = (props) => {
-// 	const [movies, setMovies] = useState([
-// 		{ name: 'Pulp fiction', price: '234', id: 23456 },
-// 		{ name: 'Kill Bill 2', price: '567', id: 456 },
-// 		{ name: 'Batman', price: '4567', id: 2346 },
-// 	])
-
-// 	const [background, setBackground] = useState('green')
-
-// 	return (
-// 		<MovieContext.Provider value={[movies, setMovies, background, setBackground]}>
-// 			{props.children}
-// 		</MovieContext.Provider>
-// 	)
-// }
-
-// const Movie = () => {
-// 	return (
-// 		<div>
-// 			<MovieData />
-// 		</div>
-// 	)
-// }
-
-// const MovieData = () => {
-// 	const [movies, setMovies] = useContext(MovieContext)
-
-// 	return (
-// 		<div>
-// 			{movies.map(movie => {
-// 				return <p 
-// 					className='bg-blue-600 text-white text-xl p-4'
-// 					key={movie.id}>
-// 					{movie.name}
-// 				</p>
-// 			})}
-// 		</div>
-// 	)
-// }
-
-// const Items = () => {
-// 	const [movies, setMovies, background] = useContext(MovieContext)
-
-// 	return (
-// 		<div className='bg-red-600 text-white text-xl p-4'>
-// 			ALL MOVIES COUNT: {movies.length}
-// 			{movies.map(movie => {
-// 				return <div key={movie.id} style={{ backgroundColor: `${background}`}}>
-// 					<div>{movie.name}</div>
-// 					<div>{movie.price}</div>
-// 					<div>{movie.id}</div>
-// 				</div>
-// 			})}
-// 		</div>
-// 	)
-// }
-
-// const AddMovie = () => {
-// 	const [movies, setMovies] = useContext(MovieContext)
-// 	const [name, setName] = useState('')
-// 	const [price, setPrice] = useState('')
-
-// 	const nameInputHandler = e => setName(e.target.value)
-// 	const priceInputHandler = e => setPrice(e.target.value)
-
-// 	const addMovieHandler = e => {
-// 		e.preventDefault()
-// 		setMovies(prev => [...prev, { name: name, price: price, id: Math.floor((1 + Math.random()) * 0x10000)  }])
-// 	}
-
-// 	return (
-// 		<form onSubmit={addMovieHandler} className='flex flex-co gap-2 bg-blue-800 text-black p-4'>
-// 			<input onChange={nameInputHandler} type="text" value={name} name='name' />
-// 			<input onChange={priceInputHandler} type="text" value={price} name='price' />
-// 			<button>Submit</button>
-// 		</form>
-// 	)
-// }
-
-// const App = () => {
-//   return (
-// 	<MovieProvider>
-// 		<Movie />
-// 		<Items />
-// 		<AddMovie />
-// 	</MovieProvider>
-//   )
-// }
-
-// export default App
-
-import React from "react";
-import Patients from "./components/Patients";
-const App = () => {
-	return (
-		<Patients />
+	return(
+		<MovieContext.Provider value={[movies, setMovies]}>
+			{props.children}
+		</MovieContext.Provider>
 	)
+}
+
+const Movie = () => {
+	const [movies, setMovies] = useContext(MovieContext)
+	return (
+		<div>
+			{movies.map(movie => (
+				<div key={movie.id} className='border-solid border-2 border-black'>
+					<p>ID: {movie.id}</p>
+					<p>NAME: {movie.id}</p>
+					<p>PRICE:{movie.id}</p>
+				</div>
+			))}
+		</div>
+	)
+}
+
+const Nav = () => {
+	const [movies, setMovies] = useContext(MovieContext)
+
+	return (
+		<p className='bg-red-200'>NUMBER OF MOVIES: {movies.length}</p>
+	)
+}
+
+const App = () => {
+	
+	
+	return(
+		<MovieProvider>
+			<Movie />
+			<Nav />
+		</MovieProvider>
+	)
+	
 }
 
 export default App;
