49b957d9aaec43b0fb25ec35125b981966283780
zrvnarmarin <76marin76@gmail.com>
2023-01-02 00:39:43 +0100

    header finished

--- test-react/src/components/Layout/Header.jsx
+++ test-react/src/components/Layout/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import HeaderCartButton from './HeaderCartButton.jsx'
 import MealsImage from '../../assets/meals.jpg'
 
 const Header = (props) => {
   return (
     <>
-        <header className=''>
-            <h1></h1>React MNeals
-            <button>Cart</button>
+        <header className='bg-orange-700 flex justify-around p-4'>
+            <h1 className='text-5xl font-medium text-white'>React Meals</h1>
+            <HeaderCartButton />
         </header>
-        <div>
-            <img src={MealsImage} alt="Table fuul of food" />
+        <div className=''>
+            <img src={MealsImage} className='w-[100%] h-[300px] object-cover' alt="Table fuul of food" />
         </div>
     </>
   )
---
+++ test-react/src/components/Layout/HeaderCartButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CartIcon from '../../assets/cart.svg'
+
+const HeaderCartButton = () => {
+  return (
+    <button className='flex flex-row gap-4 items-center text-white text-xl shadow-xl font-medium bg-orange-400 rounded-3xl px-4'>
+        <img className='h-[25px] w-[25px]' src={CartIcon} />
+        <span className=''>Your Cart</span>
+        <span className='bg-orange-300 rounded-xl px-3'>3</span>
+    </button>
+  )
+}
+
+export default HeaderCartButton;
