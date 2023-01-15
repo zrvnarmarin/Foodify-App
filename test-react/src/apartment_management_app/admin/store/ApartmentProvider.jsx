cb9f08814e059329135a81bbf1cfeaf74641d41f
zrvnarmarin <76marin76@gmail.com>
2023-01-15 23:52:19 +0100

    cleanup

--- test-react/src/apartment_management_app/admin/store/ApartmentProvider.jsx
+++ test-react/src/apartment_management_app/admin/store/ApartmentProvider.jsx
@@ -17,7 +17,7 @@ const apartmentReducer = (state, action) => {
             return {...state, apartments: state.apartments.filter(apartment => apartment.id !== action.payload)}
         }
         case ACTIONS.SHOW_APARTMENT_INFO: {
-            // ovdje treba ici modal za svaki poseni apartman 
+            // ovdje treba ici modal za svaki posebni apasrtman
             return {...state}
         }
     }
